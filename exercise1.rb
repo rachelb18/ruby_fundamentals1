603f1186d6cb69024786e0bb08cb17642083dbcd
rachelb18 <rachelkk18@gmail.com>
2018-12-16 20:45:29 -0500

    ruby_fundamentals1

--- exercise1.rb
+++ exercise1.rb
@@ -1,4 +1,5 @@
 # 2 + 3
+# puts = put string
 
 puts 2 + 3
 
@@ -8,5 +9,32 @@ puts 2 + 3
 
 #puts 2 puts 3 puts 2 + 3- will give us an error, need to have everything on seperate line 
 
- 
+ puts 'Hello world'
+ puts "Hello world"
+# no diff between single and double qoutes
 
+puts "Betty's pie shop"
+puts 'Betty\'s pie shop'
+
+# ^ since above has 2 apostrophes we need to put dash so Ruby understands how to read it.
+# The combination of the backslash followed by the single quote is called an escape sequence.
+#Using double quotes for this string allows us to avoid having to use an escape sequence.
+
+puts "Ada Lovelace lived for #{1852-1815} years."
+# Ada Lovelace live for 37 years.
+
+# \" double quote
+#\\ single backslash
+#\a bell/alert
+#\b backspace
+#\r carriage return
+#\n newline
+#\s space
+#\t tab
+
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbyeworld"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
+
+#puts "Rachel\bToronto"
