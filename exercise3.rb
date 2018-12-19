d4cf385ccf4c5d636466371d93f018804e2878e0
rachelb18 <rachelkk18@gmail.com>
2018-12-19 10:07:17 -0500

    Loop

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,34 @@
 puts "whats your name?"
+user_name = gets.chomp
+puts "Hello, #{user_name}"
+puts "how old are you?"
+user_age = gets.chomp
+puts "Hello, #{user_name} you are #{user_age} years of age."
+
+#gets vs gets.chomp You may have also seen gets.chomp
+#in other Ruby tutorials. 
+#chomp removes the unwanted line break 
+#(AKA new line AKA enter) 
+#character from the end of your user's input. 
+#In irb, try using both gets on its own 
+#as well as gets.chomp and look for the difference 
+#between the values that they return.
+#Don't forget to commit your work again!
+
+#if statement, like so:
+#number = gets.to_i # the user types in a number
+#if number > 0
+#  puts "#{number} is positive" # this line executes if the user enters a positive number
+#else
+#  puts "#{number} is negative" # this line executes if the user enters a negative number
+#end
+
+#You may find yourself expecting a boolean expression to be false rather than true. Instead of writing:
+#if x != 10
+#  puts "I get printed!"
+#end
+#You can instead use unless, which is equivalent to "if not":
+#unless x == 10
+#  puts "I get printed!"
+#end
+#It accomplishes the same thing, but now it reads more like English!
---
+++ exercise4.rb
@@ -0,0 +1,66 @@
+#puts "Enter a number"
+#number = gets.chomp.to_i
+
+#if number >= 100
+ #   puts "thats a big number"
+#else 
+#    puts "why not dream a little bigger?"
+#end
+
+#puts "Enter your age"
+#user_age = gets.chomp.to_i
+#my_age = 23 
+#difference = user_age - my_age
+#puts "#{difference} is how any years apart we are."
+#if  user_age > 105
+#    puts "I'm not sure I believe you"    
+#end    
+
+
+# my_name = "Rachel"
+# puts "Enter your name"
+# user_name = gets.chomp
+
+
+# if my_name == user_name
+#     puts "We have the same name!"
+# end
+
+# puts "enter your name"
+# user_name = gets.chomp
+# name_length = user_name.length 
+# if name_length > 10
+#     puts "hi #{user_name}"
+# elsif name_length < 10
+#     puts "hello #{user_name}"
+# else 
+#     puts "hey #{user_name}"   
+# end 
+
+# secret_number = 8
+# puts "Enter a number"
+# user_number = gets.chomp.to_i
+
+# if user_number == secret_number
+#     puts "You win!"
+# elsif user_number < secret_number
+#     puts "So close"
+# else
+#    puts "Try again"    
+# end   
+
+# counter = 1
+# while counter > 4
+#     puts "counter currently at #{counter}"
+#     counter +=1 # increase the value of counter by 1
+# end
+
+# counter = 3
+# until counter == 0
+#     puts "counter currently at #{counter}."
+#     counter -=1
+# end
+
+
+
+
